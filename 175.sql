c0e64be0af7fa911ac0b8a3f6aef7cd95bfd6111
yunsheng0204 <yunsheng1223@gmail.com>
2025-01-23 23:13:38 +0800

    First Push

--- 175.sql
+++
@@ -1,3 +0,0 @@
-# Write your MySQL query statement below
-select `firstName`,  `lastName`, `city`,  `state` from Person Left Outer Join Address
-On Person.personId = Address.personId
--- 176.sql
+++
@@ -1,2 +0,0 @@
-# Write your MySQL query statement below
-Select (Select Distinct `salary` from Employee order by `salary` desc LIMIT 1 offset 1) as SecondHighestSalary
