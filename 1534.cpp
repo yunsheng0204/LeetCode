2befa75134683a4907ec3ecddc318aa1641999f4
yunsheng0204 <yunsheng1223@gmail.com>
2025-04-14 10:58:03 +0800

    Daily Question of 2025/04/14

---
+++ 1534.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    public:
+        int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+            int len = arr.size();
+            int ans = 0;
+    
+            for(int i = 0; i<len ;i++)
+            {
+                for(int j = i + 1; j<len; j++)
+                {
+                    for(int k = j + 1; k<len; k++)
+                    {
+                        if(abs(arr[i] - arr[j]) <= a  && abs(arr[j] - arr[k]) <= b  && abs(arr[i] - arr[k]) <= c )
+                        {
+                            ans++;
+                        }
+                    }
+                }
+            }
+    
+            return ans;
+        }
+    };
