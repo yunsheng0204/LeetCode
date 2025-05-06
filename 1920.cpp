5c18bec41b25be5e7ec0b106ce608b58dc42eeea
yunsheng0204 <yunsheng1223@gmail.com>
2025-05-06 09:25:26 +0800

    Daily Question of 2025/05/06

---
+++ 1920.cpp
@@ -0,0 +1,12 @@
+class Solution {
+    public:
+        vector<int> buildArray(vector<int>& nums) {
+            int len = nums.size();
+            vector<int> ans(len);
+            for(int i = 0; i<len; i++)
+            {
+                ans[i] = nums[nums[i]];
+            }
+            return ans;
+        }
+    };
