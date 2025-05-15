1cfe059308b8474af8c6d2ead7c9d685f4401a89
yunsheng0204 <yunsheng1223@gmail.com>
2025-05-15 12:18:55 +0800

    Daily Question of 2025/05/15

---
+++ 2900.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<string> getLongestSubsequence(vector<string>& words, vector<int>& groups) {
+        vector<string> ans;
+        int len = groups.size();
+
+        ans.push_back(words[0]);
+
+        for(int i = 1; i<len; i++)
+        {
+            if(groups[i] != groups[i-1])
+            {
+                ans.push_back(words[i]);
+            }
+        }
+
+        return ans;
+    }
+};
