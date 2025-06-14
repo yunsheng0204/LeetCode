7f88ff41825553edac8f984e934bf75a80b6f546
yunsheng0204 <yunsheng1223@gmail.com>
2025-06-14 13:32:56 +0800

    Daily Question of 2025/06/14

---
+++ 2566.cpp
@@ -0,0 +1,74 @@
+class Solution {
+public:
+    int minMaxDifference(int num) {
+        vector<int> n(9,0);
+        vector<int> big_n(9,0);
+        vector<int> small_n(9,0);
+        int t = 100000000;
+        int big_temp = -1, small_temp = -1;
+        int big = 0;
+        int small = 0;
+        bool check = false;
+
+        for(int i = 8; i>=0; i--)
+        {
+            n[i] = num / t;
+            // cout<<n[i]<<" ";
+            num %= t;
+            t /= 10;
+        }
+
+        big_n = n;
+        small_n = n;
+
+        for(int i = 8; i>=0; i--)
+        {
+            if(big_temp == -1)
+            {
+                if(check == false && n[i] != 0)
+                {
+                    check = true;
+                }
+                if(n[i] != 9 && check == true)
+                {
+                    big_temp = n[i];
+                    big_n[i] = 9;
+                }
+            }
+            else
+            {
+                if(n[i] == big_temp)
+                {
+                    big_n[i] = 9;
+                }
+            }
+
+            if(small_temp == -1)
+            {
+                if(n[i] != 0)
+                {
+                    small_temp = n[i];
+                    small_n[i] = 0;
+                }
+            }
+            else
+            {
+                if(n[i] == small_temp)
+                {
+                    small_n[i] = 0;
+                }
+            }
+        }
+
+
+        for(int i = 8; i>=0; i--)
+        {
+            big = 10 * big + big_n[i];
+            small = 10 * small + small_n[i];
+        }
+
+        cout<<big<<" "<<small;
+
+        return big - small;
+    }
+};
