4f598ed11a5e68f1c3ee06a1494650e394fc66d1
yunsheng0204 <yunsheng1223@gmail.com>
2026-09-09 08:08:43 +0800

    Daily Question of 2026/09/08

---
+++ 3870.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        if(n < 1000)
+        {
+            return 0;
+        }
+        else
+        {
+            return n - 999;
+        }
+    }
+};
